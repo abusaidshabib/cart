068509c69044ad2e2a833770188fbf8a8af2921d
abusaidshabib <abusaidshabib712@gmail.com>
2025-03-07 10:55:30 -0500

    hero change 2

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 import EED from "@/assets/home/hero/EED.png";
 import TYHMD from "@/assets/home/hero/TYHMD.png";
 
 const Hero = () => {
-    return (
-      <div className="grid grid-cols-4 w-5/6 mx-auto p-5 gap-5">
-        <div className="bg-neural rounded-lg p-5">
+  return (
+    <div className="grid grid-cols-4 w-5/6 mx-auto p-5 gap-5">
+      <div className="bg-neural rounded-lg p-5 relative flex items-end">
+        <div className="absolute top-5">
           <p className="font-black text-4xl">Elegance in Every Drop</p>
-          <img src={EED} alt="" />
         </div>
-        <div className="col-span-2 rounded-lg p-5 bg-primary/30">
-          <p className="font-black text-4xl">Elegance in Every Drop</p>
-          <img src={TYHMD} alt="" />
+        <img src={EED} alt="" />
+      </div>
+      <div className="bg-primary/30 rounded-lg p-5 relative flex items-end col-span-2">
+        <div className="l absolute top-5">
+          <p className="font-black text-4xl pb-2">Elegance in Every Drop</p>
+          <p>
+            Elevate your space with stylish, functional and trend-forward
+            pieces.
+          </p>
         </div>
+        <img src={TYHMD} alt="" />
+      </div>
+      <div className="bg-primary/30 rounded-lg p-5 relative flex items-end">
         <div></div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Hero;
